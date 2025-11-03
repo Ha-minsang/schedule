040539fbf455e14235757f551225625d9cf2a694
Ha-minsang <gkalstkd@gmail.com>
2025-11-03 16:11:21 +0900

    기본 구조 설계
    createdAt과 modifiedAt 사용을 위해 코드 추가

---
+++ src/main/java/com/example/schedule/entity/Schedule.java
@@ -0,0 +1,37 @@
+package com.example.schedule.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "schedules")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 30, nullable = false)
+    private String title;
+    @Column(length = 200, nullable = false)
+    private String contents;
+    @Column(nullable = false)
+    private String writer;
+    @Column(nullable = false)
+    private String password;
+
+    public  Schedule(String title, String contents, String writer, String password) {
+        this.title = title;
+        this.contents = contents;
+        this.writer = writer;
+        this.password = password;
+    }
+
+    public void updateSchedule(String title, String writer) {
+        this.title = title;
+        this.writer = writer;
+    }
+}
