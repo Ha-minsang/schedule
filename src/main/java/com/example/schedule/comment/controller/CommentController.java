05ce4c5ad0fe9260f729d6bbf2d41bbab9ef8eb4
Ha-minsang <gkalstkd@gmail.com>
2025-11-04 20:44:46 +0900

    주석 추가

--- src/main/java/com/example/schedule/comment/controller/CommentController.java
+++ src/main/java/com/example/schedule/comment/controller/CommentController.java
@@ -8,24 +8,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 public class CommentController {
 
     private final CommentService commentService;
 
+    // CREATE 새로운 comment 생성
     @PostMapping("/schedules/{scheduleId}/comments")
     public ResponseEntity<CreateCommentResponse> createComment(@PathVariable Long scheduleId, @RequestBody CreateCommentRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(commentService.saveComment(scheduleId, request));
     }
 
+    // UPDATE comment 수정
     @PutMapping("/schedules/{scheduleId}/comments/{commentsId}")
     public ResponseEntity<UpdateCommentResponse> updateSchedule(@PathVariable Long scheduleId, Long commentId, @RequestBody UpdateCommentRequest request) {
         return ResponseEntity.status(HttpStatus.OK).body(commentService.updateComment(scheduleId, commentId, request));
     }
 
+    // DELETE comment 삭제
     @DeleteMapping("/schedules/{scheduleId}/comments/{commentsId}")
     public ResponseEntity<Void> deleteComment(@PathVariable Long scheduleId, Long commentId, @RequestBody DeleteCommentRequest request) {
         commentService.deleteComment(scheduleId, commentId, request);
--- src/main/java/com/example/schedule/comment/entity/Comment.java
+++ src/main/java/com/example/schedule/comment/entity/Comment.java
@@ -14,20 +14,30 @@ import lombok.NoArgsConstructor;
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     public class Comment extends BaseEntity {
 
+        // PK
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Column(name = "comments_id")
         private Long id;
+
+        // 댓글 내용
         @Column(length = 200, nullable = false)
         private String contents;
+
+        // 댓글 작성자
         @Column(nullable = false)
         private String writer;
+
+        // 비밀번호
         @Column(nullable = false)
         private String password;
+
+        // 다대일 관계 FK
         @ManyToOne
         @JoinColumn(name = "schedules_id", nullable=false)
         private Schedule schedule;
 
+        // 생성자
         public Comment(String contents, String writer, String password, Schedule schedule) {
             this.contents = contents;
             this.writer = writer;
@@ -35,6 +45,7 @@ import lombok.NoArgsConstructor;
             this.schedule = schedule;
         }
 
+        // 세터
         public void updateComment(String contents, String writer) {
             this.contents = contents;
             this.writer = writer;
